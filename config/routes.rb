8a31a21b126911d9dea7c54ed6f7384cb1926e68
sabina barcucci <me@sabinabar.cc>
2021-01-09 15:19:09 +0100

    adding read and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ ALL
+  get '/tasks', to: "tasks#index"
+
+ # CREATE
+  get '/tasks/new', to: "tasks#new", as: :new
+  post '/task', to: "tasks#create"
+
+  # READ ONE
+  get '/tasks/:id', to: "tasks#show", as: :singleitem
+  post '/tasks', to: "tasks#create"
+
 end
